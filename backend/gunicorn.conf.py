d9bab890fe4e80f5e6fa1ab57f0df30e5e3ceb82
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2020-03-24 00:07:19 +0100

    Use pseudo-threads for gunicorn

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,4 +1,6 @@
 import multiprocessing
 
 bind = "backend:8000"
-workers = multiprocessing.cpu_count() * 3 + 1
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = gevent
+worker_connections = 1000
