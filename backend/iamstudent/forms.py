3f1ad8e6f15febcda6e9c79b9c2362d9912bb846
jonathan.sauder <jonathan.sauder@student.hpi.de>
2020-03-24 00:12:02 +0100

    Ghettoest Datepicker ever

--- backend/iamstudent/forms.py
+++ backend/iamstudent/forms.py
@@ -144,7 +144,7 @@ class StudentForm(forms.ModelForm):
                 css_class='form-row'
             ),
             Row(
-                Column('availability_start', css_class='form-group col-md-6 mb-0'),
+                Column('availability_start',  input_type="date", css_class='form-group col-md-6 mb-0'),
                 Column('semester', css_class='form-group col-md-4 mb-0'),
                 Column('immatrikuliert', css_class='form-group col-md-2 mb-0'),
                 css_class='form-row'
