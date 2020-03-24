89a25a4bfd5f8ce9f220c81824d5d64e9f562766
jonathan.sauder <jonathan.sauder@student.hpi.de>
2020-03-24 19:19:55 +0100

    work on login etc

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
-    path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),name='password_change_done'),
-    #path('password_change',auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),name='password_change_form'),
-    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),name='password_reset_form'),
-    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),name='password_reset_done'),
+    path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done_.html'),name='password_change_done'),
+    path('password_change',auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form_.html'),name='password_change_form'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form_.html'),name='password_reset_form'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done_.html'),name='password_reset_done'),
     path('login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
 
     path('', include('django.contrib.auth.urls')),
