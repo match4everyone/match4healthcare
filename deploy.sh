c8bb48edf4576a69064b6b1877181b61c04f4a19
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2020-03-25 03:31:52 +0100

    Add compilation of messages

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker exec --env PYTHONPATH="/matchmedisvsvirus-backend:$PYTHONPATH" backend django-admin makemessages
+docker exec --env PYTHONPATH="/matchmedisvsvirus-backend:$PYTHONPATH" backend django-admin compilemessages
 docker exec backend python3 manage.py migrate
 docker exec backend python3 manage.py collectstatic --no-input
