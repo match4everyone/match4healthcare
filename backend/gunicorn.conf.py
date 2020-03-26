a4935797e53bd980c42a63ae989df1b273a3e487
Bjarne Sievers <jellyfishcoding@live.de>
2020-03-26 14:13:02 +0100

    Untested access logging. GDPR conform
    because logs only status, status line, datetime and route without query params

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -4,3 +4,6 @@ bind = "backend:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 worker_connections = 1000
+
+accesslog = 'access-logfile gunicorn-access-logfile-match4healthcare.txt'
+access_log_format = '%(t)s|"%(r)s"|%(s)s|%(T)s'
