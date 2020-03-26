63c9ca94f40b969b83b3d8838649171d460f978d
Bjarne Sievers <jellyfishcoding@live.de>
2020-03-26 14:33:15 +0100

    Tested conf file

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,9 +1,10 @@
-import multiprocessing
+import multiprocessing, os
+from match4healthcare.settings.common import BASE_DIR
 
 bind = "backend:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 worker_connections = 1000
 
-accesslog = 'access-logfile gunicorn-access-logfile-match4healthcare.txt'
+accesslog = os.path.join(BASE_DIR, 'run', 'gunicorn-access.log')
 access_log_format = '%(t)s|"%(r)s"|%(s)s|%(T)s'
