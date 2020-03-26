45817985427ee238b14db46e70fa26d102e6f28b
Freya Behrens <behrefr@gmail.com>
2020-03-26 15:41:42 +0100

    Ugly fix

--- backend/apps/iamstudent/forms.py
+++ backend/apps/iamstudent/forms.py
@@ -232,35 +232,89 @@ class EmailForm(forms.Form):
 class StudentFormEditProfile(StudentForm):
     def __init__(self, *args, **kwargs):
         super(StudentFormEditProfile, self).__init__(*args, **kwargs)
+
         self.helper.layout = Layout(
+            HTML("<h2 class='form-heading'>{}</h2>".format(_("Persönliche Informationen"))),
             Row(
                 Column('name_first', css_class='form-group col-md-6 mb-0'),
                 Column('name_last', css_class='form-group col-md-6 mb-0'),
                 css_class='form-row'
             ),
             Row(
-                Column('plz', css_class='form-group col-md-6 mb-0'),
-                Column('countrycode', css_class='form-group col-md-6 mb-0'),
+                Column('phone_number', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+
+            HTML("<hr style='margin-top: 30px; margin-bottom:30px;'><h2 class='form-heading'>{}</h2>".format(
+                _("Über deinen Einsatz"))),
+            Row(
+                Column('plz', css_class='form-group col-md-4 mb-0'),
+                Column('countrycode', css_class='form-group col-md-4 mb-0'),
+                Column('umkreis', css_class='form-group col-md-4 mb-0'),
                 css_class='form-row'
             ),
             Row(
                 Column('availability_start', css_class='form-group col-md-6 mb-0'),
-                #Column('semester', css_class='form-group col-md-4 mb-0'),
-                #Column('immatrikuliert', css_class='form-group col-md-2 mb-0'),
                 css_class='form-row'
             ),
+
+            HTML("<h5 style='margin-top:20px'>{}</h5>".format(_("Wunscheinsatzort"))),
             Row(
-                Column('phone_number', css_class='form-group col-md-6 mb-0'),
+                Column('wunsch_ort_arzt', css_class='form-group col-md-6 mb-0'),
+                Column('wunsch_ort_gesundheitsamt', css_class='form-group col-md-6 mb-0'),
+                Column('wunsch_ort_krankenhaus', css_class='form-group col-md-6 mb-0'),
+                Column('wunsch_ort_pflege', css_class='form-group col-md-6 mb-0'),
+                Column('wunsch_ort_rettungsdienst', css_class='form-group col-md-6 mb-0'),
+                Column('wunsch_ort_labor', css_class='form-group col-md-6 mb-0'),
+                Column('wunsch_ort_apotheke', css_class='form-group col-md-6 mb-0'),
+                Column('wunsch_ort_ueberall', css_class='form-group col-md-6 mb-0'),
+
                 css_class='form-row'
             ),
             Row(
                 Column('braucht_bezahlung', css_class='form-group col-md-6 mb-0'),
                 css_class='form-row'
+            ), Row(
+                Column('zeitliche_verfuegbarkeit', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ), Row(
+                Column('unterkunft_gewuenscht', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
             ),
-            HTML("<h2>{}</h2>".format(_("Berufsausbildung"))),
-            # TODO: alle neuen felder hier auch hinzufügen!!!!!
-                        Submit('submit', _('Daten aktualisieren',), css_class='btn blue text-white btn-md'),
-                  )
+            Div(
+                HTML("<hr style='margin-top: 30px; margin-bottom:30px;'><h2 class='form-heading'>{}</h2>".format(
+                    _("Berufsausbildung"))),
+                Row(*[Column('ausbildung_typ_%s' % k.lower(), css_class='ausbildung-checkbox form-group col-md-6 mb-0',
+                             css_id='ausbildung-checkbox-%s' % AUSBILDUNGS_IDS[k]) for k in
+                      AUSBILDUNGS_TYPEN.keys()]),
+                css_id='div-berufsausbildung-dropdown',
+            ),
+            *[
+                Div(
+                    HTML("<h4>{}</h4>".format(_(form_labels['ausbildung_typ_%s' % ausbildungstyp.lower()]))),
+                    Row(*[
+                        Column(button_group('ausbildung_typ_%s_%s' % (ausbildungstyp.lower(), f.lower())),
+                               css_class='form-group col-md-6 mb-0', css_id=f.replace('_', '-'))
+                        for f in felder.keys()
+                    ]), css_id='div-ausbildung-%s' % AUSBILDUNGS_IDS[ausbildungstyp]
+                    , css_class='hidden ausbildung-addon'
+                )
+                for ausbildungstyp, felder in AUSBILDUNGS_TYPEN.items() if len(felder) != 0
+            ],
+            'sonstige_qualifikationen'
+            ,
+            HTML('<hr style="margin-top: 20px; margin-bottom:30px;">'),
+            HTML('<p class="text-left">'),
+            'datenschutz_zugestimmt',
+            HTML("</p>"),
+            HTML('<p class="text-left">'),
+            'einwilligung_datenweitergabe',
+            HTML("</p>"),
+            HTML('<div class="registration_disclaimer">{}</div>'.format(_(
+                'Die Bereitstellung unseres Services erfolgt unentgeltlich. Mir ist bewusst, dass die Ausgestaltung des Verhältnisses zur zu vermittelnden Institution allein mich und die entsprechende Institution betrifft. Insbesondere Art und Umfang der Arbeit, eine etwaige Vergütung und vergleichbares betreffen nur mich und die entsprechende Institution. Eine Haftung des Vermittlers ist ausgeschlossen.'))),
+            Submit('submit', _('Registriere mich'), css_class='btn blue text-white btn-md'),
+        )
+
 
 
 
