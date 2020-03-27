0d8a0a17e02f14e55700856cbca185e2f1509aad
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2020-03-27 07:27:45 +0100

    Only execute if necessary

--- backend/apps/mapview/utils.py
+++ backend/apps/mapview/utils.py
@@ -5,22 +5,24 @@ from math import radians, sin, cos, asin, sqrt
 import pgeocode
 plzs = {}
 
-current_location = dirname(abspath(__file__))
+def read_plz():
+    if not plzs:
+        current_location = dirname(abspath(__file__))
 
-for countrycode in ["DE", "AT"]:
-    with open(join(current_location, f'files/{countrycode}.csv'), encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        plzs[countrycode] = {}
-        for row in reader:
-            for plz in row["plz"].split(","):
-                try:
-                    plzs[str(countrycode)][plz] = (float(row["lon"]), float(row["lat"]), row["ort"])
-                except Exception as e:
-                    print("Warning: count not make PLZ entry for", row)
-    df = pgeocode.Nominatim(countrycode)._data_frame.dropna()[["postal_code", "longitude", "latitude", "place_name"]].values
-    for plz, lon, lat, ort in df:
-        if plz not in plzs[countrycode]:
-            plzs[countrycode][plz] = (lon, lat, ort)
+        for countrycode in ["DE", "AT"]:
+            with open(join(current_location, f'files/{countrycode}.csv'), encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
+                plzs[countrycode] = {}
+                for row in reader:
+                    for plz in row["plz"].split(","):
+                        try:
+                            plzs[str(countrycode)][plz] = (float(row["lon"]), float(row["lat"]), row["ort"])
+                        except Exception as e:
+                            print("Warning: count not make PLZ entry for", row)
+            df = pgeocode.Nominatim(countrycode)._data_frame.dropna()[["postal_code", "longitude", "latitude", "place_name"]].values
+            for plz, lon, lat, ort in df:
+                if plz not in plzs[countrycode]:
+                    plzs[countrycode][plz] = (lon, lat, ort)
 
 def haversine(lon1, lat1, lon2, lat2):
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
@@ -31,6 +33,7 @@ def haversine(lon1, lat1, lon2, lat2):
 
 
 def get_plzs_close_to(countrycode, plz, distance_in_km):
+    read_plz()
     lon1, lat1, _ = plzs[countrycode][plz]
 
     close = []
