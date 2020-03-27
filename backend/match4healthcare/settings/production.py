db98e38deea535fc350669c0d40312394c135821
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-03-27 06:15:53 +0100

    Use sendgrid api instead of SMTP for less overhead

--- backend/match4healthcare/settings/production.py
+++ backend/match4healthcare/settings/production.py
@@ -62,11 +62,11 @@ LOGGING = {
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 # Normal SMTP
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 # Using the API
-# EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
