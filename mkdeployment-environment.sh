36473518c74b76440339ff8c64414c60baf019ba
Malte Zacharias <me@malte-zacharias.de>
2020-03-28 03:27:09 +0000

    fixed typo

--- mkdeployment-environment.sh
+++ mkdeployment-environment.sh
@@ -29,8 +29,8 @@ curl https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz |tar -xzC "${GOROOT}" 
 
 echo -e "\n\nInstalling webhook"
 go get github.com/adnanh/webhook && echo -e "Installation\t$OK" || { echo -e "Installation\t$ERROR"; exit 1; }
-git clone -q b "${PREDEPLOY_BRANCH}" --depth 1 "${REPO}" "${SERVERHOME}-predeploy" && echo -e "Git Clone ${PREDEPLOY_BRANCH} -> ${SERVERHOME}-predeploy\t$OK" || { echo -e "Git Clone\t$ERROR"; exit 1; }
-git clone -q -b "${DEPLOY_BRANCH}"   --depth 1 "${REPO}" "${SERVERHOME}-deploy"    && echo -e "Git Clone ${DEPLOY_BRANCH}    -> ${SERVERHOME}-deploy   \t$OK" || { echo -e "Git Clone\t$ERROR"; exit 1; }
+git clone -q -b "${PREDEPLOY_BRANCH}" --depth 1 "${REPO}" "${SERVERHOME}-predeploy" && echo -e "Git Clone ${PREDEPLOY_BRANCH} -> ${SERVERHOME}-predeploy\t$OK" || { echo -e "Git Clone\t$ERROR"; exit 1; }
+git clone -q -b "${DEPLOY_BRANCH}"    --depth 1 "${REPO}" "${SERVERHOME}-deploy"    && echo -e "Git Clone ${DEPLOY_BRANCH}    -> ${SERVERHOME}-deploy   \t$OK" || { echo -e "Git Clone\t$ERROR"; exit 1; }
 
 echo "Creating Config files and folders"
 ln -s "${GOPATH}/bin/webhook" "${TOOLS}"
