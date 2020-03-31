5abcf60d5749cdc0b863075031cf41b37364d40d
Malte Zacharias <me@malte-zacharias.de>
2020-03-31 23:18:53 +0200

    Add facility counter

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -278,8 +278,12 @@ class UserCountView(APIView):
     """
 
     def get(self, request, format=None):
-        user_count = User.objects.count()
-        content = {'user_count': user_count}
+        supporter_count = User.objects.filter( is_student__exact = True, validated_email__exact = True ).count()
+        facility_count =  User.objects.filter( is_hospital__exact = True, validated_email__exact = True ).count()
+        content = {
+            'user_count': supporter_count,
+            'facility_count': facility_count
+        }
         return JsonResponse(content)
 
 
