f11933be67e575d135dc63428ae47d156ea1681f
Freya <behrefr@gmail.com>
2020-04-11 12:56:48 +0200

    Add system check to deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ docker exec --env PYTHONPATH="/match4healthcare-backend:$PYTHONPATH" backend dja
 docker exec --env PYTHONPATH="/match4healthcare-backend:$PYTHONPATH" backend django-admin compilemessages
 docker exec backend python3 manage.py collectstatic --no-input
 docker exec backend python3 manage.py migrate
+docker exec backend python3 manage.py check
 # Restart container AFTER static files have been collected
 docker-compose -f docker-compose.dev.yml -f docker-compose.prod.yml down
 docker-compose -f docker-compose.dev.yml -f docker-compose.prod.yml up -d
