a5dedaaca08370903072aa9871ec6ac0523999ed
Nico Hackert <n-hackert@t-online.de>
2020-04-12 22:19:37 +0200

    Added evaluation app

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source database.prod.env
-docker-compose -f docker-compose.dev.yml -f docker-compose.prod.yml exec backend sh -c 'python3 manage.py dumpdata > /match4healthcare-backend/backups/fixture-$(date +%F_%H%M%S).json'
-docker-compose -f docker-compose.dev.yml -f docker-compose.prod.yml exec database sh -c "pg_dumpall -U $POSTGRES_USER> /backups/pg_backup-$(date +%F_%H%M%S).sql"
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml exec backend sh -c 'python3 manage.py dumpdata > /match4healthcare-backend/backups/fixture-$(date +%F_%H%M%S).json'
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml exec database sh -c "pg_dumpall -U $POSTGRES_USER> /backups/pg_backup-$(date +%F_%H%M%S).sql"
