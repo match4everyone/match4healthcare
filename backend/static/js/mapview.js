efd1d6ad3e00fb29c836889ede17d3ae0af07165
Malte Zacharias <me@malte-zacharias.de>
2020-04-15 01:00:12 +0200

    Added feature: Clustering of Supporters in Map, custom Icon for facilities

--- backend/static/js/mapview.js
+++ backend/static/js/mapview.js
@@ -10,6 +10,7 @@ mapViewPage = {
         isHospital: true,
         createPopupTextStudent  :  (countrycode,city, plz, count, url) => '',
         createPopupTextHospital :  (countrycode,city, plz, count, url) => '',
+        facilityIcon: new L.Icon.Default(),
     },
 
     mapObject: null,
@@ -22,7 +23,7 @@ mapViewPage = {
     
         let tileLayerURL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}@2x?access_token=' + this.options.mapboxToken
         let tileLayerOptions = {
-            attribution: ' <a href="https://www.mapbox.com/about/maps/">© Mapbox</a> | <a href="http://www.openstreetmap.org/copyright">© OpenStreetMap</a> | <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>',
+            attribution: ' <a href="https://www.mapbox.com/about/maps/">© Mapbox</a> | <a href="http://www.openstreetmap.org/copyright">© OpenStreetMap</a> | <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a> | Icons by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">flaticon.com</a>',
             maxZoom: 18,
             id: 'mapbox/streets-v11',
             tileSize: 512,
@@ -53,12 +54,13 @@ mapViewPage = {
         let [ facilities, supporters ] = await Promise.all([$.get(this.options.facilitiesURL),$.get(this.options.supportersURL)])
         
         let facilityMarkers = L.layerGroup(this.createMapMarkers(facilities,(lat,lon,countrycode,city,plz,count) => {
-            return L.marker([lon,lat]).bindPopup(
+            return L.marker([lon,lat],{ icon: this.options.facilityIcon }).bindPopup(
                 this.options.createPopupTextHospital(countrycode,city, plz, count, this.options.hospitalListURL.replace("COUNTRYCODE",countrycode).replace("PLZ",plz))
             )
         }))
 
-        let supporterMarkers = L.layerGroup(this.createMapMarkers(supporters,(lat,lon,countrycode,city,plz,count) => {
+        var clusterMarkerGroup = L.markerClusterGroup();
+        var supporterMarkers = L.featureGroup.subGroup(clusterMarkerGroup, this.createMapMarkers(supporters,(lat,lon,countrycode,city,plz,count) => {
             return L.circle([lon,lat], { 
                 radius: count > 30 ? ( 2000 + 50 * count ) : ( 500 + 100 * count ),
                 color: '#ed0a71', 
@@ -66,23 +68,27 @@ mapViewPage = {
                 weight: 2 + 0.25 * count, 
                 fillOpacity: .2 
             }).bindPopup(this.options.createPopupTextStudent(countrycode,city, plz, count, this.options.supporterListURL.replace("COUNTRYCODE",countrycode).replace("PLZ",plz)))
-        }))
-        
-        var overlays = {
-            "Einrichtungen" : facilityMarkers,
-            "Helfer*innen"  : supporterMarkers
-        }
+        }));        
+
+        clusterMarkerGroup.addTo(this.mapObject)
+        supporterMarkers.addTo(this.mapObject)
+        var count = await $.getJSON("/accounts/count")
+
+        const facilitiesLayerName = `<img src="/static/img/map/facility-marker.svg"> ${count.facility_count} Einrichtungen`
+        const supportersLayerName = `<img src="/static/img/map/supporter-marker.svg"> ${count.user_count} Helfer*innen`
+        var overlays = {}
+        overlays[facilitiesLayerName] = facilityMarkers
+        overlays[supportersLayerName] = supporterMarkers
 
         facilityMarkers.addTo(this.mapObject)
-        supporterMarkers.addTo(this.mapObject)
 
-        L.control.layers(null, overlays, { collapsed: false, position: 'topleft' }).addTo(this.mapObject)
+        L.control.layers(null, overlays, { collapsed: false, position: 'topright' }).addTo(this.mapObject)
     },
 
     showUserCount: async function showUserCount() {
         var count = await $.getJSON("/accounts/count")
-        document.getElementById("user_count").textContent = count.user_count
-        document.getElementById("facility_count").textContent = count.facility_count
+        /*document.getElementById("user_count").textContent = count.user_count
+        document.getElementById("facility_count").textContent = count.facility_count*/
     },
 
     createMapMarkers : function addMapMarkers(markers, createMarkerFunction) {
