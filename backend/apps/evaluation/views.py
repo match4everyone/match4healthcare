8aca6096a3b7ba220672bc72fbfaca8fe71fb6fa
Nico <n-hackert@t-online.de>
2020-04-17 17:37:32 +0200

    Removed permission checks for evaluation

--- backend/apps/evaluation/views.py
+++ backend/apps/evaluation/views.py
@@ -1,12 +1,9 @@
 from django.views.generic import CreateView
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import render
 from . import models
 
 
-class StudentEvaluationForm(UserPassesTestMixin, CreateView):
-
-    def test_func(self):
-        return self.request.user.is_student and not self.request.user.has_evaluated
+class StudentEvaluationForm(CreateView):
 
     model = models.StudentEvaluation
     fields = '__all__'
