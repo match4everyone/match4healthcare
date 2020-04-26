26eafab1126ec9e298873e0b2ff0d5bb60f6aef4
Nico <n-hackert@t-online.de>
2020-04-26 17:39:36 +0200

    Added Completed URL; added Name to URL-Mappings

--- backend/apps/evaluation/urls.py
+++ backend/apps/evaluation/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     # path for Student-Evaluations
-    path('student/', views.StudentEvaluationForm.as_view()),
+    path('student/', views.student_evaluation, name='student_evaluation'),
     # path for Institution-Evaluations
     # path('institution/', views.InstitutionEvaluationForm.as_view()),
     # evaluation complete
-    # path('completed', views.EvaluationCompleted.as_view())
+    path('completed/', views.evaluation_completed, name='evaluation_completed')
 ]
