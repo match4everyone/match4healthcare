670510003492188b1ca16d6564c34c077bfec166
Nico <n-hackert@t-online.de>
2020-04-28 18:36:21 +0200

    Improved form layout and redirect

--- backend/apps/evaluation/views.py
+++ backend/apps/evaluation/views.py
@@ -1,9 +1,7 @@
-from django.views.generic import CreateView
 from django.shortcuts import render
-from . import models
 from .forms import StudentEvaluationForm
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpRequest
+from django.urls import reverse, path
 
 
 def student_evaluation(request):
@@ -12,16 +10,38 @@ def student_evaluation(request):
 
         if form_info.is_valid():
             form_info.save()
+            request.session['eval_completed'] = True
             return HttpResponseRedirect(reverse('evaluation_completed'))
 
     else:
-        form_info = StudentEvaluationForm()
-        context = {
-            'form_info': form_info,
-        }
+        form_info = StudentEvaluationForm(initial={})
+
     form_info.helper.form_tag = False
-    return render(request, 'evaluation/studentevaluation_form.html', context)
+    return render(request, 'evaluation/studentevaluation_form.html', {'form_info': form_info})
+
+
+def institution_evaluation(request):
+    #TODO add code for institutions
+
+    if request.method == 'POST':
+        form_info = StudentEvaluationForm(request.POST)
+
+        if form_info.is_valid():
+            form_info.save()
+            request.session['eval_completed'] = True
+            return HttpResponseRedirect(reverse('evaluation_completed'))
+
+    else:
+        form_info = StudentEvaluationForm(initial={})
+
+    form_info.helper.form_tag = False
+    return render(request, 'evaluation/institutionevaluation_form.html', {'form_info': form_info})
 
 
 def evaluation_completed(request):
-    return render(request, 'evaluation/evaluation_completed.html')
+
+    if request.session.get('eval_completed', False):
+        del request.session['eval_completed']
+        return render(request, 'evaluation/evaluation_completed.html')
+
+    return HttpResponseRedirect('/')
