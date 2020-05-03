a570434a217631e9e100eedf84241db8f5ef7e30
Bjarne Sievers <jellyfishcoding@live.de>
2020-05-03 13:51:45 +0200

    Check if black lets lines be 100 chars long

--- backend/apps/accounts/utils.py
+++ backend/apps/accounts/utils.py
@@ -10,18 +10,13 @@ from django.contrib.auth.forms import PasswordResetForm
 from django.conf import settings
 
 
-def generate_random_username(
-    length=16, chars=ascii_lowercase + digits, split=4, delimiter="-"
-):
+def generate_random_username(length=16, chars=ascii_lowercase + digits, split=4, delimiter="-"):
 
     username = "".join([choice(chars) for i in range(length)])
 
     if split:
         username = delimiter.join(
-            [
-                username[start : start + split]
-                for start in range(0, len(username), split)
-            ]
+            [username[start : start + split] for start in range(0, len(username), split)]
         )
 
     try:
