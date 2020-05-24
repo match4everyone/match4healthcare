69b1b28fc9c7d42cb980ff176d62904e1c7b28e2
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-05-25 00:52:26 +0200

    Apply 'STATIC_HACK' to trick the IDEs and linters to load the submodules

--- backend/apps/accounts/models/__init__.py
+++ backend/apps/accounts/models/__init__.py
@@ -1,3 +1,15 @@
 from match4healthcare.utils.imports import import_submodules
 
-import_submodules(globals(), __name__, __path__)
+STATIC_HACK = False
+if STATIC_HACK:
+    """
+    This code is never executed, but it is needed to trick IDEs and Python linters
+    to register the dynamically loaded submodules.
+    The actual modules are loaded dynamically during startup
+    Inspired by https://github.com/celery/kombu/blob/4644a5e9400beac6668f326c16078286f7d60b64/kombu/__init__.py#L34
+    """
+    from .newsletter import *  # noqa
+    from .newsletter_approved_by import *  # noqa
+    from .user import *  # noqa
+
+import_submodules(globals(), __name__, __path__)  # noqa: F405
--- backend/apps/ineedstudent/models/__init__.py
+++ backend/apps/ineedstudent/models/__init__.py
@@ -1,3 +1,13 @@
 from match4healthcare.utils.imports import import_submodules
 
-import_submodules(globals(), __name__, __path__)
+STATIC_HACK = False
+if STATIC_HACK:
+    """
+    This code is never executed, but it is needed to trick IDEs and Python linters
+    to register the dynamically loaded submodules.
+    The actual modules are loaded dynamically during startup
+    Inspired by https://github.com/celery/kombu/blob/4644a5e9400beac6668f326c16078286f7d60b64/kombu/__init__.py#L34
+    """
+    from .hospital import *  # noqa
+
+import_submodules(globals(), __name__, __path__)  # noqa: F405
