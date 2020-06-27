fffd6e6841d79724bcb446256b73386b4e6df2f1
Malte Zacharias <me@malte-zacharias.de>
2020-06-27 07:42:38 +0200

    Move collectstatic to entry point
    This was necessary because the collection of statics depends on the
    BACKEND configuration. We only have that in production config right
    now. A (better?) alternative would be to add whitenoise in dev
    configuration and collect statics during the build process.
    Need to test if this will cause problems in dev as runserver
    is used to serve the files and I need to check if that honors
    the configured backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,7 +10,13 @@ if ! [[ -z "${PRODUCTION}" ]]; then
   done
 fi
 
-python3 manage.py migrate
-python3 manage.py check
+# While collectstatic should rather run in the Dockerfile build
+# it depends on the right DJANGO_SETTINGS_MODULE as it is subject to the
+# STATICFILES_STORAGE setting. Whitenoise is only configured in production as of now
+# so we would need to build the image in production mode, this would require setting a DJANGO_SECRET key
+echo PYTHONPATH: $PYTHONPATH
+django-admin collectstatic --no-input
+django-admin migrate
+django-admin check
 
 exec "$@"
