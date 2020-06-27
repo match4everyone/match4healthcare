9e50654803154ee3110a2b688c81c627d1f2789b
Malte Zacharias <me@malte-zacharias.de>
2020-06-27 07:29:42 +0200

    Move DB connectivity check to entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -e -o pipefail
+
+if ! [[ -z "${PRODUCTION}" ]]; then
+  echo Running in production mode, waiting for database to come up
+  # Moved from docker-compose, as this is needed for migrate
+  while ! (< /dev/tcp/database/5432) &> /dev/null; do
+    echo "Waiting for database..."
+    sleep 1
+  done
+fi
+
 python3 manage.py migrate
 python3 manage.py check
 
