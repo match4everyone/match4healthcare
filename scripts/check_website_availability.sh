a38980b05ad69f51a4ef93215e85f3da127aff16
Malte Zacharias <me@malte-zacharias.de>
2020-07-02 04:01:24 +0200

    Update log location in travis scripts

--- scripts/check_website_availability.sh
+++ scripts/check_website_availability.sh
@@ -26,7 +26,7 @@ failed=False
 URL=localhost:8000
 
 # Log file location
-ERR_LOG_PATH=./run/match4healthcare.json.error.log
+ERR_LOG_PATH=./run/log/match4healthcare.json.error.log
 
 # docker-compose to run command in backend
 DC_EXEC="docker-compose -f docker-compose.yml -f docker-compose.prod.yml exec backend bash -c "
