5d86841fb530588acce37b5668cf174452a02986
Malte Zacharias <me@malte-zacharias.de>
2020-07-02 04:36:47 +0200

    Wait for backend check for connectivity

--- scripts/wait_for_backend.sh
+++ scripts/wait_for_backend.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
 while [ -z "$(docker-compose -f docker-compose.yml -f docker-compose.prod.yml ps --services --filter "status=running"|grep backend)" ]; do
-    sleep 1
+    sleep .2
 done
+
+echo -n "Waiting for backend"
+while ! (< /dev/tcp/127.0.0.1/8000) &> /dev/null; do
+  echo -n .
+  sleep .2
+done
+echo " OK"
