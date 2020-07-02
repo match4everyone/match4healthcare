780ff44f591da0b138cc140bd05deb5c44fbd6f7
Malte Zacharias <me@malte-zacharias.de>
2020-07-02 03:44:59 +0200

    Move collectstatic to docker build process
    Was moved to entrypoint before because we were unaware that collectstatic needs to run in the production context for static file post processing. Now that we've figured that out we can let it run as part of the image build process

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -12,12 +12,7 @@ if ! [[ -z "${PRODUCTION}" ]]; then
   echo " OK"
 fi
 
-# While collectstatic should rather run in the Dockerfile build
-# it depends on the right DJANGO_SETTINGS_MODULE as it is subject to the
-# STATICFILES_STORAGE setting. Whitenoise is only configured in production as of now
-# so we would need to build the image in production mode, this would require setting a DJANGO_SECRET key
 echo PYTHONPATH: $PYTHONPATH
-django-admin collectstatic --no-input
 django-admin migrate
 django-admin check
 
