cb09e9b307f8d636ae5aaaba4b6eea75845fd263
Malte Zacharias <me@malte-zacharias.de>
2020-07-02 05:23:47 +0200

    Wait_for_backend check log files
    The previous version would detect the container as running too early. Need to check the logfile of the backend docker until gunicorn signals that it's ready and listening. Otherwise the website will not yet be up for the next script.

--- scripts/wait_for_backend.sh
+++ scripts/wait_for_backend.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+echo -n "Waiting for backend to be running"
 while [ -z "$(docker-compose -f docker-compose.yml -f docker-compose.prod.yml ps --services --filter "status=running"|grep backend)" ]; do
+    echo -n .
     sleep .2
 done
-
-echo -n "Waiting for backend"
-while ! (< /dev/tcp/127.0.0.1/8000) &> /dev/null; do
-  echo -n .
-  sleep .2
-done
 echo " OK"
+
+WAIT_FOR="\[INFO\] Listening at"
+echo -n "Wait for '$WAIT_FOR' in backend log"
+while [ -z "$(docker-compose -f docker-compose.yml -f docker-compose.prod.yml logs backend|grep "${WAIT_FOR}")" ]; do
+    echo -n .
+    sleep 1
+done
+echo " Found"
